73821f259bf6efb15aa3c941865f3acae5a781ff
Liyb65 <2625715894@qq.com>
2025-07-16 08:52:11 +0800

    add c file

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+// This is a simple C program that prints "Hello, World!" to the console.
+// It is written in C17 standard, which is a version of the C programming language standard.
